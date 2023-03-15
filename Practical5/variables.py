d4b7c808129e1de8297e1c7316580ba38bcf46b8
superFF-qwq <1836569635@qq.com>
2023-03-15 08:43:16 +0100

    modify

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -3,12 +3,12 @@ b = -118.24
 c = 116.39
 d = a - b
 e = c - a
-if (d > e): #Rob travels further to Haining
-	print("Rob travels further to Haining")
-if (d < e): #Rob travels further to Los Angeles
-	print("Rob travels further to Los Angeles")
+if (d > e): #Rob travelled further to Haining
+	print("Rob travelled further to Haining")
+if (d < e): #Rob travelled further to Los Angeles
+	print("Rob travelled further to Los Angeles")
 #Answer:
-#Rob travels further to Haining
+#Rob travelled further to Haining
 
 
 X = True
