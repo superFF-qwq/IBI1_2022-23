9b8ec820f94a832d48e8c0946e3c5f1846c80717
superFF-qwq <1836569635@qq.com>
2023-03-15 08:39:08 +0100

    add comments

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -1,8 +1,8 @@
-generation = 1
-rabbits = 2
+generation = 1 # initialise generation = 1
+rabbits = 2 # initialise rabbits number = 2
 while(True):
-	if(rabbits > 100):
+	if(rabbits > 100): # if rabbits exceed 100, then break
 		break
 	generation += 1
-	rabbits = rabbits * 2
-print("At generation "+str(generation)+" over 100 rabbits have been born")
+	rabbits = rabbits * 2 # every two rabbits will have 2 babies
+print("At generation "+str(generation)+" over 100 rabbits have been born") # print answer
