55b961914c2be953b500258aa793d5c54af43853
superFF-qwq <1836569635@qq.com>
2023-03-21 08:23:19 +0100

    draft3 - done

--- Practical6/costs.py
+++ Practical6/costs.py
@@ -2,3 +2,16 @@ costs=[1,8,15,7,5,14,43,40]
 costs.sort() #costs=sorted(costs)
 print(costs)
 
+import numpy as np
+import matplotlib.pyplot as plt
+plt.boxplot(costs,
+	vert = True,
+	whis = 1.5,
+	patch_artist = True,
+	meanline = False,
+	showbox = True,
+	showcaps = True,
+	showfliers = True,
+	notch = False
+	)
+plt.show()
