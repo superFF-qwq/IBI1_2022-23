de4a4eaf3cc3202012b1327946d0ab89d1fb712f
superFF-qwq <1836569635@qq.com>
2023-03-21 08:44:59 +0100

    change pattern

--- Practical6/costs.py
+++ Practical6/costs.py
@@ -6,13 +6,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.boxplot(costs,
 	widths = 0.6,
-	vert = False,
+	vert = True,
 	whis = 1.5,
 	patch_artist = True,
+	showmeans = True,
 	meanline = False,
 	showbox = True,
 	showcaps = True,
 	showfliers = True,
-	notch = False
+	notch = False,
+	boxprops={"facecolor": "C0", "edgecolor": "black","linewidth": 0.5, "alpha":0.4}
 	)
 plt.show()
