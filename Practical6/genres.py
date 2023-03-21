c6f4884e14942a5428d7cabd3e9042a0d52d0d43
superFF-qwq <1836569635@qq.com>
2023-03-21 08:10:31 +0100

    draft3-done

--- Practical6/genres.py
+++ Practical6/genres.py
@@ -6,7 +6,7 @@ explode = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 
 plt.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=False, startangle=90)
 plt.axis('equal')
-plt.show
+plt.show()
 
 genres = {'Comedy':73, 'Action':42, 'Romance':38, 'Fantasy':28, 'Science-fiction':22, 'Horror':19, 'Crime':18, 'Documentary':12, 'History':8, 'War': 7}
 
