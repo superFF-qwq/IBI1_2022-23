d03bbc9b5153e90587c44d8400d56c56a89e67ca
superFF-qwq <1836569635@qq.com>
2023-03-22 09:52:44 +0100

    done

--- Practical6/costs.py
+++ Practical6/costs.py
@@ -1,21 +1,26 @@
-costs=[1,8,15,7,5,14,43,40] #initialise
-costs.sort() #costs=sorted(costs)
-print(costs) #print answer
-
 import numpy as np
 import matplotlib.pyplot as plt
 #use abbreviations
-plt.boxplot(costs,
-	widths = 0.6,
-	vert = True,
-	whis = 1.5,
-	patch_artist = True,
-	showmeans = True,
-	meanline = False,
-	showbox = True,
-	showcaps = True,
-	showfliers = True,
-	notch = False,
-	boxprops={"facecolor": "C0", "edgecolor": "black","linewidth": 0.5, "alpha":0.4}
-	) # set some parameters in the boxplot, and the others are default values
+
+_costs = [1,8,15,7,5,14,43,40]
+_OlympicGames = ["Los Angeles 1984", "Seoul 1988", "Barcelona 1992", "Atlanta 1996", "Sydney 2000", "Athens 2003", "Beijing 2008", "London 2012"]
+#initialise data
+
+data = zip(_costs, _OlympicGames)
+data = sorted(data)
+
+costs = [item[0] for item in data]
+OlympicGames = [item[1] for item in data]
+
+#print(costs)
+#print(OlympicGames)
+
+n = len(data)
+ind = np.arange(n)
+
+plt.bar(ind, costs, width = 0.35)
+plt.ylabel("Costs")
+plt.title("Costs of Olympic Games")
+plt.xticks(ind, OlympicGames)
+
 plt.show()
