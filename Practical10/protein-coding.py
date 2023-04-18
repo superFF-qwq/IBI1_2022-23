a911a5c7c3c3e4fae9d10778fc6f3fcee7d6a19a
superFF-qwq <1836569635@qq.com>
2023-04-18 07:22:33 +0100

    done

--- Practical10/protein-coding.py
+++ Practical10/protein-coding.py
@@ -1,21 +1,26 @@
 ## for task3: protein-coding capability	calculator
 
 def checkcoding(seq):
-    for i in range(0,len(seq)):
-        if 'a'<=seq[i] and seq[i]<='z':
-            seq[i]=seq[i]-'a'+'A'
+    seq = seq.upper()
+    ##print(seq)
+    ## change lower case to upper case
     ATG = seq.find('ATG')
     TGA = seq.find('TGA')
-    distance = TGA - ATG
+    distance = TGA - ATG - 3
+    ## distance does NOT contain the codons themselves
     percentage = 1.0 * distance / len(seq)
+    ## calculate proportion
     if distance > len(seq)*0.5:
         label = 'protein-coding'
     elif distance < len(seq)*0.1:
         label = 'non-coding'
     else:
         label = 'unclear'
+    ## check the label
     return percentage, label
 
-seq = 'ATGGTAAGAAGCTGA'
+##seq = 'ATGGTAAGAAGCTGA'
+##seq = 'acatggtagtgata'
+seq = 'aCaTgGtAgTgAtA'
 percentage, label = checkcoding(seq)
 print(percentage, label)
--- Practical10/triathlon.py
+++ Practical10/triathlon.py
@@ -11,8 +11,8 @@ class triathlon(object):
         ## time unit : second
     def checkmember(self):
         self.total = self.swim + self.cycle + self.run
-        print(self.first_name+' '+self.last_name+' '+self.location+' '+ \
-              str(self.swim)+' '+str(self.cycle)+' '+str(self.run)+' '+str(self.total))
+        print(self.first_name, self.last_name, self.location, \
+              self.swim, self.cycle, self.run, self.total)
 
 champion = triathlon('Hao', 'Miao', 'Spain', 48*60+36, 4*3600+34*60+14, 3*3600+6)
 champion.checkmember()
